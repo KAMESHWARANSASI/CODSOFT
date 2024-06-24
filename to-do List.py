4f317881c67336de865eccc5a93c1469ff2ffe5a
KAMESHWARANSASI <kameshwarankamesh6833@gmail.com>
2024-06-24 19:35:40 +0530

    Update to-do List.py

--- to-do List.py
+++ to-do List.py
@@ -1,5 +1,7 @@
-"""A To-Do List application is a useful project that helps users manage and organize their tasks efficiently. This project aims to create a command-line or GUI-based application using Python, 
-allowing users to create, update, and track their to-do lists"""
+"""A To-Do List application is a useful project that helps users manage and
+organize their tasks efficiently. This project aims to create a command-line or 
+GUI-based application using Python, allowing users to create, update, and track
+their to-do lists"""
 def main():
     tasks = []
 
